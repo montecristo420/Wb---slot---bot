c9fff343084aad26e8ecb7117f6c8e6b20bb2285
montecristo420 <montecrisro76@gmail.com>
2025-07-06 18:18:45 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -7,7 +7,6 @@ import os
 
 def check_slots_and_book():
     options = Options()
-    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
